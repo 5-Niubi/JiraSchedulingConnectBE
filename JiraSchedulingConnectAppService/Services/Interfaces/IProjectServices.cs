ecaac0383f2341dc431c37523a4b5f5cc0f6f9a5
area1110 <khanhhuy1110@gmail.com>
2023-06-22 00:47:25 +0700

    Adding create project controller

--- JiraSchedulingConnectAppService/Services/Interfaces/IProjectServices.cs
+++ JiraSchedulingConnectAppService/Services/Interfaces/IProjectServices.cs
@@ -5,7 +5,8 @@ namespace JiraSchedulingConnectAppService.Services.Interfaces
 {
     public interface IProjectServices
     {
-         public Task< PagingResponseDTO<ProjectListHomePageDTO>> GetAllProject( int currentPage);
-         public Task CreateProject(ProjectsListCreateProject.Request projectRequest);
+        public Task<PagingResponseDTO<ProjectListHomePageDTO>> GetAllProject(int currentPage);
+        public Task<ProjectDetailDTO> CreateProject(ProjectsListCreateProject.Request projectRequest);
+        public Task<ProjectDetailDTO> GetProjectDetail(int projectId);
     }
 }
