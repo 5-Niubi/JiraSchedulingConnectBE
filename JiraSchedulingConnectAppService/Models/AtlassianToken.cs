28c92c23642b3bac0bb9fcb76c35e674a0f089c5
area1110 <khanhhuy1110@gmail.com>
2023-06-25 14:55:53 +0700

    Update DB

--- JiraSchedulingConnectAppService/Models/AtlassianToken.cs
+++ JiraSchedulingConnectAppService/Models/AtlassianToken.cs
@@ -7,11 +7,13 @@ public partial class AtlassianToken
 {
     public int Id { get; set; }
 
-    public string? AccountId { get; set; }
+    public string? AccountInstalledId { get; set; }
 
     public string? CloudId { get; set; }
 
     public string? AccessToken { get; set; }
 
     public string? RefressToken { get; set; }
+
+    public virtual ICollection<AccountRole> AccountRoles { get; set; } = new List<AccountRole>();
 }
--- JiraSchedulingConnectAppService/Services/AuthenticationService.cs
+++ JiraSchedulingConnectAppService/Services/AuthenticationService.cs
@@ -1,6 +1,7 @@
 ﻿using JiraSchedulingConnectAppService.DTOs.Authentication;
 using JiraSchedulingConnectAppService.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System.Text;
 using System.Text.Json;
 
@@ -22,61 +23,84 @@ namespace JiraSchedulingConnectAppService.Services
         async public Task<Object> InitAuthen(string code, string state)
         {
             // Handle State
-            string stateStr = Encoding.UTF8.GetString(Convert.FromBase64String(state));
-            var stateContextObject = JsonSerializer.Deserialize<StateContextObjectCallback>(stateStr);
-
-            var jwtManager = new JWTManagerService(config);
-            // *Token
-            var jwtToken = jwtManager.Authenticate(stateContextObject.accountId, stateContextObject.cloudId);
-
-
-            var reponseTokenFirstPhase = await InitialAcess(code);
-            var accessiableResourceResponseDTO = await GetUserAccessiableResource(reponseTokenFirstPhase.access_token);
-
-            var tokenFromDB = await db.AtlassianTokens.FirstOrDefaultAsync(e => e.CloudId == stateContextObject.cloudId && e.AccountId == stateContextObject.accountId);
-            if (tokenFromDB == null)
+            db.Database.BeginTransaction();
+            try
             {
-                var token = new AtlassianToken()
+
+                string stateStr = Encoding.UTF8.GetString(Convert.FromBase64String(state));
+                var stateContextObject = JsonSerializer.Deserialize<StateContextObjectCallback>(stateStr);
+
+                var jwtManager = new JWTManagerService(config);
+                // *Token
+                var jwtToken = jwtManager.Authenticate(stateContextObject.accountId, stateContextObject.cloudId);
+
+                var reponseTokenFirstPhase = await InitialAcess(code);
+                var accessiableResourceResponseDTO = await GetUserAccessiableResource(reponseTokenFirstPhase.access_token);
+
+                var tokenFromDB = await db.AtlassianTokens
+                    .FirstOrDefaultAsync(e => e.CloudId == stateContextObject.cloudId && e.AccountInstalledId == stateContextObject.accountId);
+                if (tokenFromDB == null)
                 {
-                    AccountId = stateContextObject?.accountId,
-                    CloudId = stateContextObject?.cloudId,
-                    AccessToken = reponseTokenFirstPhase.access_token,
-                    RefressToken = reponseTokenFirstPhase.refresh_token
+                    var token = new AtlassianToken()
+                    {
+                        AccountInstalledId = stateContextObject?.accountId,
+                        CloudId = stateContextObject?.cloudId,
+                        AccessToken = reponseTokenFirstPhase.access_token,
+                        RefressToken = reponseTokenFirstPhase.refresh_token
+                    };
+                    var tokenInserted = db.AtlassianTokens.Add(token).Entity;
+                    db.SaveChanges();
+
+                    var firstAccount = new AccountRole()
+                    {
+                        AccountId = stateContextObject?.accountId,
+                        TokenId = tokenInserted.Id
+                    };
+                    db.AccountRoles.Add(firstAccount);
+                }
+                else
+                {
+                    tokenFromDB.AccessToken = reponseTokenFirstPhase.access_token;
+                    tokenFromDB.RefressToken = reponseTokenFirstPhase.refresh_token;
+                }
+                db.SaveChanges();
+
+                // Hand saking with forge client
+                var request = new HttpRequestMessage(HttpMethod.Post, stateContextObject.triggerUrl);
+                var webTriggerRequestDTO = new WebTriggerCallbackBodyDTO.Request
+                {
+                    token = jwtToken
                 };
-                db.AtlassianTokens.Add(token);
-            }
-            else
-            {
-                tokenFromDB.AccessToken = reponseTokenFirstPhase.access_token;
-                tokenFromDB.RefressToken = reponseTokenFirstPhase.refresh_token;
-            }
-            db.SaveChanges();
+                var jsonWebTriggerRequestDTO = JsonSerializer.Serialize(webTriggerRequestDTO);
+                var content = new StringContent(jsonWebTriggerRequestDTO, null, "application/json");
 
-            // Hand saking with forge client
-            var request = new HttpRequestMessage(HttpMethod.Post, stateContextObject.triggerUrl);
-            var webTriggerRequestDTO = new WebTriggerCallbackBodyDTO.Request
+                request.Content = content;
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                return reponseTokenFirstPhase;
+            }
+            catch (Exception ex)
             {
-                token = jwtToken
-            };
-            var jsonWebTriggerRequestDTO = JsonSerializer.Serialize(webTriggerRequestDTO);
-            var content = new StringContent(jsonWebTriggerRequestDTO, null, "application/json");
-
-            request.Content = content;
-            var response = await client.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-            return reponseTokenFirstPhase;
+                db.Database.RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                db.Database.CloseConnection();
+            }
         }
 
         async public Task<RepsoneAccessToken?> InitialAcess(string code)
         {
             var request = new HttpRequestMessage(HttpMethod.Post, "https://auth.atlassian.com/oauth/token");
+            var domain = config.GetValue<string>("Environment:SelfDomain");
             var exchangeTokenDTO = new ExchangeAccessTokenDTO
             {
                 grant_type = "authorization_code",
                 client_id = "wDzzxAZSrrM9DtPwZ295BMT3YoFR6KeD",
                 client_secret = "ATOAtGENwZ61eAzU1RL-zw2t1e66ITe2XsF-Nj_-SNYuhQZuyDVeA82EytqZf5xUQebU1B2E2744",
                 code = code,
-                redirect_uri = "http://localhost:14849/Authentication/Callback",
+                redirect_uri = $"{domain}/Authentication/Callback",
             };
             var jsonExchangeDTO = JsonSerializer.Serialize(exchangeTokenDTO);
             var content = new StringContent(jsonExchangeDTO, null, "application/json");
