d5023874950758206e8dcd06f84bb8658994fdec
area1110 <khanhhuy1110@gmail.com>
2023-06-26 15:20:00 +0700

    Adding GetAllProjectNonPaging api

--- JiraSchedulingConnectAppService/Services/Interfaces/IProjectServices.cs
+++ JiraSchedulingConnectAppService/Services/Interfaces/IProjectServices.cs
@@ -5,7 +5,8 @@ namespace JiraSchedulingConnectAppService.Services.Interfaces
 {
     public interface IProjectServices
     {
-        public Task<PagingResponseDTO<ProjectListHomePageDTO>> GetAllProject(int currentPage);
+        public Task<PagingResponseDTO<ProjectListHomePageDTO>> GetAllProjectsPaging(int currentPage, string? projectName);
+        public Task<List<ProjectListHomePageDTO>> GetAllProjects(string? projectName);
         public Task<ProjectDetailDTO> CreateProject(ProjectsListCreateProject.Request projectRequest);
         public Task<ProjectDetailDTO> GetProjectDetail(int projectId);
     }
