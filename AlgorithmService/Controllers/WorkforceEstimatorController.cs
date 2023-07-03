21a9d9f960ada36bd94fc5848092228c27ba4c24
Manh Pham <70097274+Phammanh26@users.noreply.github.com>
2023-07-03 16:40:46 +0700

    create algorithm estimate pharse 1

---
+++ AlgorithmService/Controllers/WorkforceEstimatorController.cs
@@ -0,0 +1,71 @@
+﻿using AlgorithmServiceServer;
+using Microsoft.AspNetCore.Mvc;
+using RcpspEstimator;
+
+namespace AlgorithmServiceServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkforceEstimatorController : ControllerBase
+    {
+
+        List<List<int>> TaskAdjacency = new List<List<int>>()
+                                                {
+                                                    new List<int> {0, 0, 0, 0, 0},
+                                                    new List<int> {1, 0, 0, 0, 0},
+                                                    new List<int> {0, 1, 0, 0, 0},
+                                                    new List<int> {0, 1, 0, 0, 0},
+                                                    new List<int> {0, 0, 1, 1, 0}
+                                                };
+
+        List<List<int>> TaskExper = new List<List<int>>()
+        {
+            new List<int> {0,0,0},
+            new List<int> {1,0,2},
+            new List<int> {3,0,3},
+            new List<int> {2,0,4},
+            new List<int> {0,0,0}
+        };
+
+
+        List<int> TaskDuration = new List<int> { 0, 10, 1, 3, 1 };
+
+        private readonly ILogger<WorkforceEstimatorController> _logger;
+
+        public WorkforceEstimatorController(ILogger<WorkforceEstimatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetEstimateWorkforce")]
+        public IEnumerable<EstimatedWorkforce> Get()
+        {
+
+            // TODO
+            // Get data from relate tables: Task
+            // Encode to matrix TaskDuration, TaskExper, TaskAdjacency
+
+            ScheduleEstimator estimator = new ScheduleEstimator(
+                TaskDuration,
+                TaskExper,
+                TaskAdjacency
+                );
+            estimator.ForwardMethod();
+            List<List<int>> outputs = estimator.Fit();
+
+
+            // TODO
+            // Store output to Database
+
+            return Enumerable.Range(0, outputs.Count).Select(index => new EstimatedWorkforce
+            {
+                Id = 123,
+                Skills = outputs[index],
+                NumberOfWorkforce = 2
+            })
+            .ToArray();
+
+
+        }
+    }
+}
---
+++ AlgorithmService/EstimatedWorkforce.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AlgorithmServiceServer
+{
+    public class EstimatedWorkforce
+
+    {
+        public int Id { get; set; }
+        public List<int> Skills { get; set; }
+        public int NumberOfWorkforce { get; set; }
+        public string? Summary { get; set; }
+    }
+}
+
