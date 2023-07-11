b812d78b585700b5c692d42cac498d401d797112
area1110 <khanhhuy1110@gmail.com>
2023-07-11 15:18:20 +0700

    Create Get ScheduleAPI

---
+++ JiraSchedulingConnectAppService/Controllers/ScheduleController.cs
@@ -0,0 +1,50 @@
+﻿using JiraSchedulingConnectAppService.Services;
+using JiraSchedulingConnectAppService.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelLibrary.DTOs;
+using UtilsLibrary.Exceptions;
+
+namespace JiraSchedulingConnectAppService.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService scheduleService;
+        public ScheduleController(IScheduleService scheduleService)
+        {
+            this.scheduleService = scheduleService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetSchedules(int parameterId)
+        {
+            try
+            {
+                var response = await scheduleService.GetSchedules(parameterId);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                var response = new ResponseMessageDTO(ex.Message);
+                return BadRequest(response);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSchedule(int scheduleId)
+        {
+            try
+            {
+                var response = await scheduleService.GetSchedule(scheduleId);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                var response = new ResponseMessageDTO(ex.Message);
+                return BadRequest(response);
+            }
+        }
+
+    }
+}
---
+++ JiraSchedulingConnectAppService/Services/ScheduleService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using JiraSchedulingConnectAppService.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using ModelLibrary.DBModels;
+using ModelLibrary.DTOs.Algorithm;
+using UtilsLibrary.Exceptions;
+
+namespace JiraSchedulingConnectAppService.Services
+{
+    public class ScheduleService : IScheduleService
+    {
+
+        private readonly JiraDemoContext db;
+        private readonly IMapper mapper;
+        public ScheduleService(JiraDemoContext db, IMapper mapper)
+        {
+            this.db = db;
+            this.mapper = mapper;
+        }
+
+        public async Task<List<ScheduleResultSolutionDTO>> GetSchedules(int parameterId)
+        {
+            var schedule = await db.Schedules.Where(s => s.ParameterId == parameterId)
+                 .ToListAsync();
+            var scheduleDTO = mapper.Map<List<ScheduleResultSolutionDTO>>(schedule);
+            return scheduleDTO;
+        }
+
+        public async Task<ScheduleResultSolutionDTO> GetSchedule(int scheduleId)
+        {
+            var schedule = await db.Schedules.Where(s => s.Id == scheduleId)
+                 .FirstOrDefaultAsync() ??
+            throw new NotFoundException($"Can not find schedule: {scheduleId}");
+            var scheduleDTO = mapper.Map<ScheduleResultSolutionDTO>(schedule);
+            return scheduleDTO;
+        }
+    }
+}
