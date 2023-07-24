ae82362e60b2005a431974605af85afd52853cf7
Manh Pham <70097274+Phammanh26@users.noreply.github.com>
2023-07-24 07:48:51 +0700

    update workflow config

--- AlgorithmService/Program.cs
+++ AlgorithmService/Program.cs
@@ -7,77 +7,61 @@ using ModelLibrary.DBModels;
 using ModelLibrary.DTOs;
 using System.Text;
 
-namespace ConsoleApp2
-{
-    public static class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-        private static async System.Threading.Tasks.Task Main(string[] args)
+        options.TokenValidationParameters = new TokenValidationParameters
         {
-            var builder = WebApplication.CreateBuilder(args);
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+        };
+    });
 
-            // Add services to the container.
+builder.Services.AddDbContext<JiraDemoContext>(opt => opt.UseSqlServer(
+    builder.Configuration.GetConnectionString("DB")
+    )
+);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors();
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+builder.Services.AddHttpContextAccessor();
 
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                        ValidAudience = builder.Configuration["Jwt:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
-                    };
-                });
-
-            builder.Services.AddDbContext<JiraDemoContext>(opt => opt.UseSqlServer(
-                builder.Configuration.GetConnectionString("DB")
-                )
-            );
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddCors();
-            builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
-            builder.Services.AddHttpContextAccessor();
-
-            // Register services
-            builder.Services.AddTransient<IAlgorithmComputeService, AlgorithmComputeService>();
-            builder.Services.AddTransient<IEstimateWorkforceService, EstimateWorkforcService>();
+// Register services
+builder.Services.AddTransient<IAlgorithmComputeService, AlgorithmComputeService>();
+builder.Services.AddTransient<IEstimateWorkforceService, EstimateWorkforcService>();
 
 
-            var app = builder.Build();
+var app = builder.Build();
 
-            //app.Urls.Add("http://localhost:3000");
-            //app.Urls.Add("http://localhost:80");
-
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllers();
-            // Custom Config:
-            app.UseCors(opt => opt.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+//app.Urls.Add("http://localhost:3000");
+//app.Urls.Add("http://localhost:80");
 
 
-            app.Run();
-
-        }
-
-    }
-
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-   
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+// Custom Config:
+app.UseCors(opt => opt.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 
- 
+app.Run();
