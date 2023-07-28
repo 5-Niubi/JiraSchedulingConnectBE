50573d7c0173b6e7fee370bf475cd87223f6ee2f
Manh Pham <70097274+Phammanh26@users.noreply.github.com>
2023-07-28 18:38:06 +0700

    update save params api

--- ModelLibrary/DTOs/Parameters/ParameterRequestDTO.cs
+++ ModelLibrary/DTOs/Parameters/ParameterRequestDTO.cs
@@ -7,6 +7,11 @@ namespace ModelLibrary.DTOs.PertSchedule
         public int ProjectId { get; set; }
         public int Duration { get; set; }
         public float Budget { get; set; }
+
+        public int? ObjectiveTime { get; set; }
+        public int? ObjectiveCost { get; set; }
+        public int? ObjectiveQuality { get; set; }
+
         public DateTime? StartDate { get; set; }
         public DateTime? Deadline { get; set; }
         public List<ParameterResourceRequest> ParameterResources { get; set; }
