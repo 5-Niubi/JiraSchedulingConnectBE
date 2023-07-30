c206ebf221fcf8bdc07a1b3c70055e8e404927a8
area1110 <khanhhuy1110@gmail.com>
2023-07-30 16:15:12 +0700

    Add line chart and Fix https

--- ResourceAssignAdmin/wwwroot/js/site-chart.js
+++ ResourceAssignAdmin/wwwroot/js/site-chart.js
@@ -23,3 +23,32 @@ function drawUserDonutChart(totalUsersArray) {
 
     let chart = new Chart(document.getElementById("userDonut"), configChart);
 }
+
+function drawUserLineChart() {
+    const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July'
+    ];
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+    };
+
+    const configChart = {
+        type: 'line',
+        data: data,
+    };
+
+    let chart = new Chart(document.getElementById("userLine"), configChart);
+}
