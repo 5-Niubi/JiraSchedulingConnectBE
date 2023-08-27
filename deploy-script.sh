79e02d2ba10044f197596cf8d231158cdf4a55cd
PVM <70097274+Phammanh26@users.noreply.github.com>
2023-08-27 15:35:24 +0700

    update base64 for google credential

--- deploy-script.sh
+++
@@ -1,9 +0,0 @@
-cd BE_REPO 
-git checkout $branch_name && git pull 
-BACKEND_REPOSITORY="pham2604/5niubi_backend" \
-GATEWAY_REPOSITORY="pham2604/gateway_service" \
-ADMIN_REPOSITORY="pham2604/admin_webapp" \
-ASPNETCORE_ENVIRONMENT="Production" \
-VERSION="dev-least" \
-docker compose -f docker-compose.backend.yml up -d \
-sudo docker image prune -a --force && exit"
