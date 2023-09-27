2775b60aee069f988a84f6c16e042cc1033f1735
area1110 <khanhhuy1110@gmail.com>
2023-09-27 20:57:44 +0700

    Update Database schema

--- ModelLibrary/Migrations/20230927130154_UpdateLatest.Designer.cs
+++ ModelLibrary/Migrations/20230927130154_UpdateLatest.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ModelLibrary.DBModels;
 
@@ -11,9 +12,10 @@ using ModelLibrary.DBModels;
 namespace ModelLibrary.Migrations
 {
     [DbContext(typeof(WoTaasContext))]
-    partial class WoTaasContextModelSnapshot : ModelSnapshot
+    [Migration("20230927130154_UpdateLatest")]
+    partial class UpdateLatest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ModelLibrary/Migrations/20230927130154_UpdateLatest.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModelLibrary.Migrations
+{
+    public partial class UpdateLatest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "unit_salary",
+                table: "workforce",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "cost",
+                table: "schedules",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "budget",
+                table: "projects",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "budget",
+                table: "parameter",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "optimizer",
+                table: "parameter",
+                type: "int",
+                nullable: true,
+                comment: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "optimizer",
+                table: "parameter");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "unit_salary",
+                table: "workforce",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "cost",
+                table: "schedules",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "budget",
+                table: "projects",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "budget",
+                table: "parameter",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
